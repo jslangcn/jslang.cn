f09068e575c5527e86782531b5d5b5dc4f25f97c
lzw <lzwdot@qq.com>
2023-03-31 22:03:26 +0800

    init

--- src/components/home/Index.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import { loadToken, issuesList } from '../_source/serve'
-
-!(async () => {
-  await loadToken()
-  await issuesList()
-})()
-</script>
-<template></template>
